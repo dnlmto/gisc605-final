4ce7a62a699bf12a8ee1abba80edb8c285720cce
D.M <59850465+dnlmto@users.noreply.github.com>
2023-12-13 18:13:16 -0800

    Update final_a.sh
    added the shebang

--- final_a.sh
+++ final_a.sh
@@ -2,7 +2,7 @@
 # Add a file called final_a.sh to your repository
 # Please write a bash script with a single function called gisc605 that uses echo to output your CSULB username. Make sure to commit your changes to your repo.
 #####
-
+#!/bin/bash
 function gisc605() {
     echo "Daniel.Mateo01@student.csulb.edu"
 }
