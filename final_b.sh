7b5a69a251ef75121c788bafc4a3bc13d5923987
D.M <59850465+dnlmto@users.noreply.github.com>
2023-12-13 18:15:34 -0800

    Update final_b.sh
    updated cat function

--- final_b.sh
+++ final_b.sh
@@ -2,4 +2,4 @@
 # Please write a bash script with list the contents of your final_a.sh script with cat. Make sure to commit your changes to your repo.
 ###
 #!/bin/bash
-$cat final_a.sh
+cat final_a.sh
