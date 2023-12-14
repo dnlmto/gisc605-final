4180bd094672ee5b1cfcf76d6a7d557ac1cdfc83
D.M <59850465+dnlmto@users.noreply.github.com>
2023-12-13 17:57:33 -0800

    Create final_a.sh
    created a function called gisc605 that echoes my CSULB username.

---
+++ final_a.sh
@@ -0,0 +1,8 @@
+#####
+# Add a file called final_a.sh to your repository
+# Please write a bash script with a single function called gisc605 that uses echo to output your CSULB username. Make sure to commit your changes to your repo.
+#####
+
+function gisc605() {
+    echo "Daniel.Mateo01@student.csulb.edu"
+}
